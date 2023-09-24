4e8e815f00c0c5435168a883666833327e56ed7a
SandstormNick <gibbensnick@gmail.com>
2023-09-24 17:04:17 +0200

    Added start-point component

--- Roadmap/src/app/app.module.ts
+++ Roadmap/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { StartPointComponent } from './start-point/start-point.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    StartPointComponent
   ],
   imports: [
     BrowserModule,
---
+++ Roadmap/src/app/start-point/start-point.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-start-point',
+  templateUrl: './start-point.component.html',
+  styleUrls: ['./start-point.component.scss']
+})
+export class StartPointComponent {
+
+}
