fcfe0fc075134d562fee175b2e2b735f2665ce7b
SandstormNick <gibbensnick@gmail.com>
2023-09-25 20:12:26 +0200

    Added the rearrangeCardsOnInit method to rearrange cards as they are initialized in the layer component

--- Roadmap/src/app/app.component.ts
+++ Roadmap/src/app/app.component.ts
@@ -16,13 +16,23 @@ export class AppComponent {
 
   layers: Layer[] = [
     new Layer(1, [
-      new Card(1, 1, "Card 1", 0, null),
-      new Card(1, 2, "Card 2", 0, 1),
-      new Card(1, 3, "Card 3", 0, null)
+      new Card(1, 1, "Card 1", 0, null, true),
+      new Card(1, 2, "Card 2", 0, 1, false),
+      new Card(1, 3, "Card 3", 0, null, false)
     ]),
     new Layer(2, [
-      new Card(2, 4, "Card 4", 1, null),
-      new Card(2, 5, "Last card for now", 1, null)
+      new Card(2, 4, "Card 4", 1, null, true),
+      new Card(2, 5, "Card 5", 1, null, false)
+    ]),
+    new Layer(3, [
+      new Card(3, 6, "Layer 3 Card", 4, null, true)
+    ]),
+    new Layer(4, [
+      new Card(4, 7, "Card 7", 6, null, true),
+      new Card(4, 8, "Card 8", 0, 7, false)
+    ]),
+    new Layer(5, [
+      new Card(5, 9, "Card 9", 7, null, false)
     ])
   ];
 
--- Roadmap/src/app/layer/layer.component.ts
+++ Roadmap/src/app/layer/layer.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+
 import { Layer } from '../models/layer';
+import { Card } from '../models/card';
 
 @Component({
   selector: 'app-layer',
@@ -13,7 +15,34 @@ export class LayerComponent implements OnInit{
 
   ngOnInit(): void {
     this.layerTitle += ` ${this.layer.layerCount}`;
-    console.log(this.layer);
+    
+    this.layer.cards = this.rearrangeCardsOnInit(this.layer.cards);
+  }
+
+  rearrangeCardsOnInit(cards: Card[]): Card[] {
+    let rearrangedCards: Card[] = [];
+
+    let parentCards: Card[] = [];
+    let parentlessCards: Card[] = [];
+    let siblingCards: Card[] = [];
+
+    cards.forEach(card => {
+      if (card.siblingCard !== null){
+        siblingCards.push(card);
+      }
+      else if (!card.hasChildren){
+        parentlessCards.push(card);
+      }
+      else {
+        parentCards.push(card);
+      }
+    });
+
+    rearrangedCards.push(...parentlessCards);
+    rearrangedCards.push(...parentCards);
+    rearrangedCards.push(...siblingCards);
+
+    return rearrangedCards
   }
 
 }
