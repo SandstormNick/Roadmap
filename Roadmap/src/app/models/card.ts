3b1224c6de36b1743987aa8cac5f52fbab4ddad8
SandstormNick <gibbensnick@gmail.com>
2023-09-25 20:13:12 +0200

    An update to the Card model class. Added a hasChildren field

--- Roadmap/src/app/models/card.ts
+++ Roadmap/src/app/models/card.ts
@@ -3,13 +3,15 @@ export class Card {
     cardId: number;
     cardTitle: string;
     parentCard: number;
-    siblingCard?: number;
+    siblingCard?: number; //Sibling cards cannot have Parent/Children cards (for now at least)
+    hasChildren: boolean;
 
-    constructor(layer: number, cardId: number, title: string, parent: number, sibling: number) {
+    constructor(layer: number, cardId: number, title: string, parent: number, sibling: number, children: boolean) {
         this.layer = layer;
         this.cardId = cardId;
         this.cardTitle = title;
         this.parentCard = parent;
         this.siblingCard = sibling;
+        this.hasChildren = children;
     }
 }
